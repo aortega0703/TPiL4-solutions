d1b5b9e28b6aaecb7a3969004c2c7ec04aca7187
Andrés Felipe Ortega Montoya <aortega0703@gmail.com>
2023-02-27 22:15:06 -0500

    5.3 other properties (partial)

--- chapter-5.lean
+++ chapter-5.lean
@@ -21,6 +21,7 @@ example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
     apply And.intro h.left.left (And.intro h.left.right h.right)
   . intro h
     apply And.intro (And.intro h.left h.right.left) h.right.right
+
 example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
   apply Iff.intro
   . intro h
@@ -37,6 +38,7 @@ example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
       cases hr with
       | inl hrl => apply Or.inl (Or.inr hrl)
       | inr hrr => apply Or.inr hrr
+
 -- distributivity
 example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
   apply Iff.intro
@@ -48,6 +50,7 @@ example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
     cases h with
     | inl hl => apply And.intro hl.left (Or.inl hl.right)
     | inr hr => apply And.intro hr.left (Or.inr hr.right)
+
 example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
   apply Iff.intro
   . intro h
@@ -60,41 +63,44 @@ example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
     | inr hr =>
       cases h.right with
       | inl hl => apply Or.inl hl
-      | inr hrr => apply Or.inr ⟨hr, hrr⟩ 
+      | inr hrr => apply Or.inr ⟨hr, hrr⟩
+
 -- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) :=
-  Iff.intro
-    (λ hpqr : p → q → r =>
-        λ hpq : p ∧ q =>
-          hpqr hpq.1 hpq.2)
-    (λ hpqr : (p ∧ q) → r =>
-        λ hp : p =>
-          λ hq : q =>
-            hpqr ⟨hp, hq⟩)
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
-  ⟨ λ h : (p ∨ q) → r =>
-    ⟨ λ hp : p =>
-      h (Or.inl hp)
-    , λ hq : q =>
-      h (Or.inr hq) ⟩
-  , λ h : (p → r) ∧ (q → r) =>
-    λ hpq : p ∨ q =>
-      hpq.elim h.left h.right⟩
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
-  ⟨
-    λ h : ¬(p ∨ q) =>
-      ⟨ λ hp : p => h (Or.inl hp)
-      , λ hq : q => h (Or.inr hq)⟩,
-    λ h : ¬p ∧ ¬q =>
-      λ hpq : p ∨ q =>
-        hpq.elim h.left h.right
-  ⟩
-example : ¬p ∨ ¬q → ¬(p ∧ q) :=
-  λ h : ¬p ∨ ¬q =>
-    λ hpq : p ∧ q =>
-      h.elim
-        (λ hnp : ¬p => hnp hpq.left)
-        (λ hnq : ¬q => hnq hpq.right)
+example : (p → (q → r)) ↔ (p ∧ q → r) := by
+  apply Iff.intro
+  . intro h h2
+    apply (h h2.left) h2.right
+  . intro h hp hq
+    apply h (And.intro hp hq)
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := by
+  apply Iff.intro
+  . intro h
+    constructor
+    . intro hp
+      apply h (Or.inl hp)
+    . intro hq
+      apply h (Or.inr hq)
+  . intro h h2
+    apply Or.elim h2 h.left h.right
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := by
+  apply Iff.intro
+  . intro h
+    constructor
+    intro hp
+    . apply h (Or.inl hp)
+    intro hq
+    . apply h (Or.inr hq)
+  . intro h h2
+    apply h2.elim h.left h.right
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) := by
+  intro h h2
+  cases h with
+  | inl hl => apply hl h2.left
+  | inr hr => apply hr h2.right
+ 
 example : ¬(p ∧ ¬p) :=
   λ h : p ∧ ¬p => h.right h.left
 example : p ∧ ¬q → ¬(p → q) :=
@@ -185,3 +191,7 @@ example : (((p → q) → p) → p) :=
       (λ hp : p => hp)
       (λ hnp : ¬p =>
         h (λ hp : p => absurd hp hnp))
+
+example (p q r : Prop) (hp : p)
+  : (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) := by
+  admit
